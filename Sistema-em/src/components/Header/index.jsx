872e44bc468403f10c675c61af01e061f143690d
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-13 10:12:43 -0300

    add caminhos

--- Sistema-em/src/components/Header/index.jsx
+++ Sistema-em/src/components/Header/index.jsx
@@ -1,3 +1,5 @@
+import { Link, NavLink } from 'react-router-dom'
+
 import './style.css'
 
 
@@ -11,7 +13,7 @@ export function Header() {
         <img src="../images/logo-empreenda-mais.png" alt="empreenda-mais" />
 
         <ul>
-            <li>Pessoas</li>
+            <NavLink to={'/pessoas'}><li>Pessoas</li></NavLink>
             <li>Empreendimentos</li>
             <li>Atendimentos</li>
             <li>Protocolos</li>
