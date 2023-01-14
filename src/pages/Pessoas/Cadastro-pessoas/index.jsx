0fcbf5151211607176a3f9c380b975157e1b6f14
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-14 07:05:05 -0300

    add form person

--- src/pages/Pessoas/Cadastro-pessoas/index.jsx
+++ src/pages/Pessoas/Cadastro-pessoas/index.jsx
@@ -1,4 +1,4 @@
-import 'style.css'
+import './style.css'
 
 import { Link, NavLink } from 'react-router-dom'
 
@@ -9,7 +9,18 @@ export function CadasPessoas() {
     return(
         <div className='container'>
                 <h1>Cadastrando pessoa</h1>
+            <div className='formulario-pessoas'>
+                    <form action="">
+                        <div className='block'>
+                            <section>
+                            <label htmlFor="name">Nome de empreendedor:</label>
+                        <input type="text" name="" id="name" placeholder='Nome...'/>
+                            </section>
+                        </div>
 
+
+                    </form>
+            </div>
 
         </div>
 
