0eb528746a4cfca74058f5c4657ee188ce559a93
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-16 14:08:57 -0300

    add form pessoas

--- src/pages/Pessoas/Cadastro-pessoas/index.jsx
+++ src/pages/Pessoas/Cadastro-pessoas/index.jsx
@@ -16,6 +16,21 @@ export function CadasPessoas() {
                             <label htmlFor="name">Nome de empreendedor:</label>
                         <input type="text" name="" id="name" placeholder='Nome...'/>
                             </section>
+
+                            <section>
+                                <label htmlFor="tipo">Qual o tipo de pessoa? *</label>
+                                <select name="" id="tipo">
+                                    <option value="">Apoiador</option>
+                                    <option value="">Apoiado</option>
+                                </select>
+
+                            </section>
+                            <section>
+                                <label htmlFor="cpf" id='labelCPF'>CPF: *</label>
+                                <input type="text" name="" id="cpf" placeholder='CPF...'/>
+
+                            </section>
+
                         </div>
 
 
