c967c2cd8fe3f5a04c182dedcaf547350cdfd659
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-16 14:28:21 -0300

    add form

--- src/pages/Pessoas/Cadastro-pessoas/index.jsx
+++ src/pages/Pessoas/Cadastro-pessoas/index.jsx
@@ -27,10 +27,72 @@ export function CadasPessoas() {
                             </section>
                             <section>
                                 <label htmlFor="cpf" id='labelCPF'>CPF: *</label>
-                                <input type="text" name="" id="cpf" placeholder='CPF...'/>
+                                <input type="number" name="" id="cpf" placeholder='CPF...'/>
 
                             </section>
+                            <section>
+                                <label htmlFor="rg">RG: *</label>
+                                <input type="number" id='rg'/>
+                            </section>
+                            <section>
+                                <label htmlFor="email">Email:</label>
+                                <input type="email" name="" id="email" placeholder='exemplo@exemplo.com'/>
+                            </section>
 
+                        </div>
+
+                        <div className='block2'>
+                                <section>
+                                    <label htmlFor="sexo">Sexo</label>
+                                    <select name="" id="sexo">
+                                        <option value="">Masculino</option>
+                                        <option value="">Feminino</option>
+                                    </select>
+                                </section>
+                                <section>
+                                    <label htmlFor="estado">Estado Civil:</label>
+                                    <select name="" id="estado">
+                                        <option value="">Casado</option>
+                                        <option value="">Solteiro</option>
+                                        <option value="">Viúvo</option>
+                                        <option value="">Divorciado</option>
+                                    </select>
+                                </section>
+                                <section>
+                                    <label htmlFor="situacao">Situação do participante</label>
+                                    <select name="" id="situacao">
+                                        <option value="">Opcoes</option>
+                                        <option value="">Opcoes</option>
+                                        <option value="">Opcoes</option>
+                                    </select>
+                                </section>
+                                <section>
+                                    <label htmlFor="indicacao">Indicaria o programa?</label>
+                                    <select name="" id="indicacao">
+                                        <option value="">Sim</option>
+                                        <option value="">Não</option>
+                                    </select>
+                                </section>
+                                <section>
+                                    <label htmlFor="escolaridade">Escolaridade:</label>
+                                    <select name="" id="escolaridade">
+                                        <option value="">Fundamental incompleto</option>
+                                        <option value="">Fundamental completo</option>
+                                        <option value="">Médio incompleto</option>
+                                        <option value="">Médio completo</option>
+                                        <option value="">Superior incompleto</option>
+                                        <option value="">Superior completo</option>
+                                    </select>
+                                </section>
+                                <section>
+                                    <label htmlFor="renda">Renda familiar:</label>
+                                    <select name="" id="renda">
+                                        <option value="">Menor que R$1.000</option>
+                                        <option value="">Entre R$1.000 e R$2.000</option>
+                                        <option value="">Entre R$2.000 e R$5.000</option>
+                                        <option value="">Mais de R$5.000</option>
+                                    </select>
+                                </section>
                         </div>
 
 
