4a13a953cc607d6aafbb89d1c7a84008777f1451
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-17 12:00:44 -0300

    add image

--- src/pages/Home/App.jsx
+++ src/pages/Home/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 
 import './App.css'
+import ImageConstru from '../../images/contrucao.jpg'
 
 export function Home() {
   
@@ -8,7 +9,10 @@ export function Home() {
   return (
   <div className='container'>
       <h1>Home</h1>
-      
+      <div className='imageConstru'>
+      <img src={ImageConstru} alt="" srcset="" />
+
+      </div>
   </div>
   )
 }
