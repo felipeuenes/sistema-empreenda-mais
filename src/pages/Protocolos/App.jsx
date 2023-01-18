387313dd991c60e21df1f5b83dd0a9ac8d5dad53
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-18 09:03:30 -0300

    add

--- src/pages/Protocolos/App.jsx
+++ src/pages/Protocolos/App.jsx
@@ -20,6 +20,22 @@ export function Proto() {
                   <label htmlFor="data">Data do atendimento</label>
                   <input type="date" name="" id="data" />
                 </section>
+                <section>
+                  <label htmlFor="nomeEmpresa">Nome da Empresa:</label>
+                  <input type="text" name="" id="nomeEmpresa" placeholder='Nome da empresa...'/>
+                </section>
+                <section>
+                  <label htmlFor="atendimento">Tipo de atendimento:</label>
+                  <select name="" id="atendimento">
+                    <option value="150">Visitas em geral</option>
+                    <option value="250">Atendimento para prestação de serviço</option>
+                    <option value="350">Assinatura de documento em ambiente interno</option>
+                    <option value="450">Consultorias, mentorias, visitas da gerente do Empreenda Mais</option>
+                    <option value="550">Acompanhamento de especialistas ou parceiros - mentorias ou consultorias</option>
+                    <option value="650">Palestras, oficinas, outros...</option>
+                  </select>
+                </section>
+              
 
             </div>
             <div className='block2'>
