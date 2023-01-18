e2b26f3dee45f91052d6fa09b118936a2725b611
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-18 14:46:25 -0300

    add formal ou não

--- src/pages/Empreendimentos/Cadastro-empreendimentos/index.jsx
+++ src/pages/Empreendimentos/Cadastro-empreendimentos/index.jsx
@@ -1,11 +1,14 @@
 import './style.css'
 import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export function CadastroEmpre(){
 
 
-        // const [isFormal, setIsFormal] = useState;
+        const [isFormal, setIsFormal] = useState('');
+        useEffect(() =>{
+                
+        },[isFormal])
 
     return(
         <>
@@ -26,18 +29,17 @@ export function CadastroEmpre(){
                 </section>
                 <section>
                 <label htmlFor="situacao">Situação atual:</label>
-                        <select name="" id="situacao">
-                            <option value="true">Formal</option>
-                            <option value="false">Não formal</option>
-                            <option value="false">Em processo de formalização</option>
+                        <select name="" id="situacao" onChange={(e) => setIsFormal(e.target.value)}>
+                            <option value={true}>Formal</option>
+                            <option value={false}>Não formal</option>
+                            <option value={false}>Em processo de formalização</option>
                         </select>
                 </section>
+                {/* <p>{isFormal}</p> */}
+{
+        !isFormal ? 
 
-                </div>
-
-                
-                
-                <div className='bloc2'>
+        <div className='bloc2'>
                     
                <section>
                         <label htmlFor="cnpj">CNPJ:*</label>
@@ -75,6 +77,53 @@ export function CadastroEmpre(){
 
                 </div>
 
+                : <section>
+                        <label htmlFor="">teste</label>
+                </section>
+}
+
+                </div>
+
+                
+                {/* </div> */}
+                
+                {/* <div className='bloc2'>
+                    
+                    <section>
+                             <label htmlFor="cnpj">CNPJ:*</label>
+                             <input type="number" name="" id="cnpj" required/>
+                    </section>
+     
+                     <section>
+     
+                             <label htmlFor="datacpnj">Data CNPJ:</label>
+                             <input type="date" name="" id="datacpnj" />
+                     </section>
+                     <section>
+     
+                             <label htmlFor="inscricaoEs">Inscrição Estadual:*</label>
+                             <input type="number" name="" id="inscricaoEs" />
+                     </section>
+                     <section>
+     
+                             <label htmlFor="inscricaoMu">Inscrição Municipal:*</label>
+                             <input type="number" name="" id="nscricaoMu" />
+                     </section>
+                     <section>
+     
+     
+                             <label htmlFor="telefone">Telefone:</label>
+                             <input type="text" id='telefone' placeholder='(  )9 XXXX-XXXX' required/>
+                     </section>
+                     <section>
+     
+                             
+                             <label htmlFor="whats">Whatsapp:</label>
+                             <input type="text" name="" id="whats" placeholder='(  )9 XXXX-XXXX'/>
+                     </section>
+                     
+     
+                     </div> */}
 
 
                 <div className='bloc3'>
@@ -112,16 +161,16 @@ export function CadastroEmpre(){
                             <option value="">Depositos e distribuidoras</option>
                         </select>
                 </section>
-                <section>
+                {/* <section>
 
-                        {/* <label htmlFor="situacao">Situação atual:</label>
+                        <label htmlFor="situacao">Situação atual:</label>
                         <select name="" id="situacao">
                             <option value="true">Formal</option>
                             <option value="false">Não formal</option>
                             <option value="false">Em processo de formalização</option>
-                        </select> */}
+                        </select>
                        
-                </section>
+                </section> */}
                 <section>
                         <label htmlFor="nameEmpresario">Nome do empresário:</label>
                         <input type="text" name="" id="nameEmpresario" />
