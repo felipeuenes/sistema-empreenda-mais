efc5e733454e89ceb228ea9f090e69cb3bb87d14
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-20 19:12:12 -0300

    ajuste na fonte

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,14 +9,14 @@ return(
     <nav>
         <ul>
         <NavLink to="/" className={({isAtive}) => isAtive ? 'active' : ''}><li title='Página inicial'><img id='logoEM' src={logoEM}/></li></NavLink>
-            <Link to="/pessoas"><li title='Cadastro de pessoas'>PESSOAS</li></Link>
-            <Link to="/empreendimentos"><li title='Cadastro de negócios'>NEGÓCIOS</li></Link>
-            <li title='Cadastro de eventos'>EVENTOS</li>
-            <li title='Cadastro de cursos'>CURSOS</li>
-            <li title='Cadastro de mentorias'>MENTORIAS</li>
-            <li title='Cadastro de atendimentos'>ATENDIMENTOS</li>
-            <Link to="/protocolos"><li title='Cadastro de protocolos'>PROTOCOLOS</li></Link>
-            <a title='Configurações de usuário'>*USUÁRIO*</a>
+            <Link to="/pessoas"><li title='Cadastro de pessoas'>Pessoas</li></Link>
+            <Link to="/empreendimentos"><li title='Cadastro de negócios'>Negócios</li></Link>
+            <li title='Cadastro de eventos'>Eventos</li>
+            <li title='Cadastro de cursos'>Cursos</li>
+            <li title='Cadastro de mentorias'>Mentorias</li>
+            <li title='Cadastro de atendimentos'>Atendimentos</li>
+            <Link to="/protocolos"><li title='Cadastro de protocolos'>Protocolos</li></Link>
+            
         </ul>
         <div className='titulos-pagina'>
             <h1></h1>
