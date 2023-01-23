cd5daa6564d4bceed1132c802e3853e5a2909eb1
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-23 13:35:14 -0300

    ajustes gerais

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -9,11 +9,7 @@ export function Home() {
 
   return (
   <div>
-    <div className='abas'>
-          <a href="#" id='opcao1'>OPÇÃO 1</a>
-          <p>INÍCIO</p>
-          <a href="#" id='opcao2'>OPÇÃO 2</a>
-        </div>
+    
     <div className='container'>
       <div className='imageConstru'>
         <img src={ImageConstru} alt="" srcset="" />
