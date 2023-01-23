7e9da311a1748b60fa64e04738532698467e9105
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-23 11:07:44 -0300

    ajust names

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -4,11 +4,11 @@ import { Header } from '../components/Header'
 import { Home } from '../pages/Home/home.jsx'
 import { Negocios } from '../pages/negocios/negocios'
 import { Pessoas } from '../pages/Pessoas/pessoas'
-import { CadastroEmpre } from '../pages/negocios/cadastro/negociosCad'
-import { CadasPessoas } from '../pages/Pessoas/pessoas'
-import { Proto } from '../pages/Protocolos/protocoloImp'
+import { CadNegocios } from '../pages/negocios/cadastro/negociosCad'
+import { CadPessoas } from '../pages/pessoas/cadastro/pessoasCad'
+import { ImpProto } from '../pages/Protocolos/protocoloImp'
 // import { ProtoImpri } from '../pages/Protocolos/ProtocoloImpressao/App'
-import { Folha } from '../components/protocolo-folha/protocoloFolha'
+import { ProtocoloFolha } from '../components/protocolo-folha/protocoloFolha'
 
 import { Eventos } from '../pages/Eventos/App'
 import { CadastroEvento } from '../pages/Eventos/Cadastro-eventos'
@@ -24,10 +24,10 @@ export function AppRoutes() {
             <Route path={'/'} element={<Home/>}></Route>
             <Route path={'/negocios'} element={<Negocios/>}></Route>
             <Route path={'/pessoas'} element={<Pessoas/>}></Route>
-            <Route path={'/cadastro-empreendimentos'} element={<CadastroEmpre/>}></Route>
-            <Route path={'/cadaspessoa'} element={<CadasPessoas/>}/>
-            <Route path={'/protocolos'} element={<Proto/>}/>
-            <Route path={'/imprimir'} element={<Folha/>}/>
+            <Route path={'/cadastro-empreendimentos'} element={<CadNegocios/>}></Route>
+            <Route path={'/cadaspessoa'} element={<CadPessoas/>}/>
+            <Route path={'/protocolos'} element={<ImpProto/>}/>
+            <Route path={'/imprimir'} element={<ImpProto/>}/>
             <Route path={'/eventos'} element={<Eventos/>}/>
             <Route path={'/cadasEvento'} element={<CadastroEvento/>}/>
 
