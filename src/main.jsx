1bf493b0301ea9ab9dde5ae612982f8671e92a72
Jorge Alysson <jorgealysson201@gmail.com>
2023-01-23 11:31:14 -0300

    add

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import { Header } from './components/header'
 import { Footer } from './components/footer/footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
   <React.StrictMode>
     <BrowserRouter>
     <Header/>
