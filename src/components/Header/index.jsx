d14dd89559250a296e8d870179921c9d9d12820e
Jorge Alysson <jorgealysson201@gmail.com>
2023-01-25 12:01:07 -0300

    alterando "início" e "pessoas"

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,14 +10,14 @@ return(
         <ul>
         <NavLink to="/" className={({isAtive}) => isAtive ? 'active' : ''}><li title='Página inicial'><img id='logoEM' src={logoEM}/></li></NavLink>
             <Link to="/pessoas">
-                <li title='Cadastro de pessoas'><img src="../../images/pessoas.png" alt=""/>Pessoas</li>
+                <li title='Cadastro de pessoas'><img src="../../images/pessoas.png" alt=""/>PESSOAS</li>
             </Link>
-            <Link to="/negocios"><li title='Cadastro de negócios'>Negócios</li></Link>
-            <Link to='/eventos'><li>Eventos</li></Link>
-            <li>Cursos</li>
-            <li>Mentorias</li>
-            <Link to="/atendimento"><li>Atendimentos</li></Link>
-            <Link to="/protocolos"><li>Protocolos</li></Link>
+            <Link to="/negocios"><li title='Cadastro de negócios'>NEGÓCIOS</li></Link>
+            <Link to='/eventos'><li>EVENTOS</li></Link>
+            <li>CURSOS</li>
+            <li>MENTORIAS</li>
+            <Link to="/atendimento"><li>ATENDIMENTOS</li></Link>
+            <Link to="/protocolos"><li>PROTOCOLOS</li></Link>
             
         </ul>
         
--- src/pages/pessoas/index.jsx
+++ src/pages/pessoas/index.jsx
@@ -7,9 +7,56 @@ import './pessoas.css'
 
 export function Pessoas() {
 return (
-        <div className='container'>
-            <h1>Pessoas</h1>
-            
-        </div>
+<div className='container'>
+    <div className='secao-titulo'>
+        <h1>Pessoas</h1>
+        <Link to="/cadasPessoa"><button id='btn-cadpessoa'><span>&#43; </span>Cadastrar nova</button></Link>
+    </div>
+    
+    <div className='lista-pessoas'>
+        <section className='pessoa-bloco1'> 
+            <button>Pesquisar</button>
+            <input className='barra-pesquisa' type="text" placeholder='Pesquisar'/>
+            <button className='btn-filtrar'>Filtrar colunas</button>
+            <section className='opcoes-filtro'>            
+                <div><input name="agente" type="checkbox"/>Agente responsável</div>
+                <div><input name="adesao" type="checkbox"/>Adesão</div>
+                <div><input name="codigo" type="checkbox"/>Código</div>
+                <div><input name="nome" type="checkbox"/>Nome completo</div>
+                <div><input name="cpf" type="checkbox"/>CPF</div>
+                <div><input name="rg" type="checkbox"/>RG</div>
+                <div><input name="telefone" type="checkbox"/>Telefone</div>
+                <div><input name="email" type="checkbox"/>Email</div>
+                <div><input name="endereco-res" type="checkbox"/>Endereço residencial</div>
+            </section>
+        </section>
+
+       <section className='pessoa-bloco2'> 
+       <table>
+        <thead> 
+            <th>ADESÃO</th>
+            <th>NOME COMPLETO</th>
+            <th>CÓDIGO</th>
+            <th>CPF</th>
+        </thead>
+        <tbody>
+          <tr>
+              <td>SIM</td>
+              <td>JORGE ALYSSON</td>
+              <td>0001</td>
+              <td>000.000.000-00</td>
+          </tr>
+          <tr>
+            <td>NÃO</td>
+            <td>LUANA FERREIRA</td>
+            <td>0002</td>
+            <td>000.000.000-01</td>
+          </tr>
+        </tbody>
+       </table>
+       </section>
+
+    </div>
+</div>
 )
 }
