3cb685509efab96d52af146ab2ab2c7d943932f3
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-26 08:28:40 -0300

    add

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -13,22 +13,22 @@ export function Header() {
                         <li title='Página inicial'><img id='logoEM' src={logoEM} /></li>
                     </NavLink>
                     <Link to="/pessoas">
-                        <li title='Lista de pessoas'><img src="/src/images/icone-pessoas.png" alt="" />Pessoas</li>
+                        <li title='Lista de pessoas'><img src="/src/images/icone-pessoas1.png" alt="" />Pessoas</li>
                     </Link>
                     <Link to="/negocios">
-                        <li title='Lista de negócios (empreendimentos)'><img src="/src/images/icone-negocios.png" alt="" />Negócios</li>
+                        <li title='Lista de negócios (empreendimentos)'><img src="/src/images/icone-negocios1.png" alt="" />Negócios</li>
                     </Link>
                     <Link to="/eventos">
                         <li title='Lista de eventos'><img src="/src/images/icone-eventos.png" alt="" />Eventos</li>
                     </Link>
                     <Link to="/">
-                        <li title='Lista de cursos'><img src="/src/images/icone-cursos.png" alt="" />Cursos</li>
+                        <li title='Lista de cursos'><img src="/src/images/icone-cursos1.png" alt="" />Cursos</li>
                     </Link>
                     <Link to="/">
                         <li title='Lista de mentorias'><img src="/src/images/icone-mentorias1.png" alt="" />Mentorias</li>
                     </Link>
                     <Link to="/atendimentos">
-                        <li title='Lista de atendimentos'><img src="/src/images/icone-atendimentos.png" alt="" />Atendimentos</li>
+                        <li title='Lista de atendimentos'><img src="/src/images/icone-atendimentos1.png" alt="" />Atendimentos</li>
                     </Link>
                     <Link to="/protocolos">
                         <li title='Lista de protocolos'><img src="/src/images/icone-protocolos.png" alt="" />Protocolos</li>
