0f2c8ad8f8fba0493a50e62a8aae254373167057
Jorge Alysson <jorgealysson201@gmail.com>
2023-01-27 11:37:35 -0300

    add

--- src/pages/Eventos/App.jsx
+++ src/pages/Eventos/App.jsx
@@ -9,11 +9,11 @@ export function Eventos() {
   return (
     <div className='container'>
       <div className='secao-titulo'>
-                <img src="/src/images/logo-eventos.png" id='icone-titulo'/><h1>Eventos</h1>
-                <Link to="/cadastro-eventos"><button><img src="/src/images/icones/mais.png" id='icone-botao-cad' alt="" />Cadastrar novo</button></Link>
-            </div>
+        <img src="/src/images/logo-eventos.png" id='icone-titulo' /><h1>Eventos</h1>
+        <Link to="/cadastro-eventos"><button><img src="/src/images/icones/mais.png" id='icone-botao-cad' alt="" />CADASTRAR NOVO</button></Link>
+      </div>
 
-      <List/>
+      <List />
     </div>
   )
 }
