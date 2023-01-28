93baed62aa6293e453f04981f8c23d257dc1e399
Felipe Uenes <uenesdesigner@gmail.com>
2023-01-27 23:42:29 -0300

    add

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,33 +4,40 @@ import './home.css'
 import { Progress, Space } from 'antd'
 
 
-
 export function Home() {
-  
+
 
   return (
-  
-    
-<div className='container'>
-  <h1>INÍCIO</h1>
-      <section className='cont-estatisticas'>
-        <section>
-       <Space>
-        <section>
-        <h1>Visitas em Janeiro</h1>
-            <Progress type="dashboard" percent={30}/>
-        </section>
-        <section>
-            <h1>Metas 2023</h1>
-            <Progress type="dashboard" percent={5} gapDegree={30}/>
-        </section>
 
 
-       </Space>
+    <div className='container'>
+      <h1>INÍCIO</h1>
+
+      <div className='cont-estatisticas'>
+        <section>
+          <Space>
+            <section>
+              <h1>Visitas em Janeiro</h1>
+              <Progress type="dashboard" percent={30} />
+            </section>
+            <section>
+              <h1>Metas 2023</h1>
+              <Progress type="dashboard" percent={5} gapDegree={30} />
+            </section>
+
+
+          </Space>
+        </section>
+        </div>
+        <div className='cont-estatisticas'>
+
+        <section>
+         
         </section>
 
-            
-        
+        </div>
+
+
         {/* <p>
           PLACEHOLDER DAS ESTATÍSTICAS<br/>
           Apresentados ao programa: X <br/>
@@ -39,10 +46,9 @@ export function Home() {
           Participantes do programa: X <br/>
           Participantes do programa: X <br/>
         </p> */}
-        
-      </section>
-</div>
-  
+
+    </div>
+
   )
 }
 
