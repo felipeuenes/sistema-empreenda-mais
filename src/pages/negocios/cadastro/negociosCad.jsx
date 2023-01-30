53e03e7e3c0c3254c2a238d6b719580bb0512308
Jorge Alysson <jorgealysson201@gmail.com>
2023-01-30 16:25:02 -0300

    add

--- src/pages/negocios/cadastro/negociosCad.jsx
+++ src/pages/negocios/cadastro/negociosCad.jsx
@@ -1,132 +1,135 @@
 //NEGÓCIOS > CADASTRO > PÁGINA
 import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import './negociosCad.css'
 
-export function CadNegocios(){
-    return(
-        <div className='container'>
-             <div className='titulo'>
-                <img src="/src/images/logo-negocios.png" id='icone-pagina'/><h1>CADASTRAR NEGÓCIO</h1>
-                <Link to="/negocios"><button><img src="/src/images/icones/icone-lista.png" id='icone-lista'/>LISTAGEM</button></Link>
-            </div>
-        <div className='formulario-empreedimento'>
+import '/src/components/formularios/formularios.css'
 
-            <form action="">
-                <div className='primeiros-inputs'>
+export function CadNegocios() {
+	return (
+		<div className='container'>
+			<div className='titulo'>
+				<img src="/src/images/logo-negocios.png" id='icone-pagina' /><h1>CADASTRAR NEGÓCIO</h1>
+				<Link to="/negocios"><button><img src="/src/images/icones/icone-lista.png" id='icone-lista' />LISTAGEM</button></Link>
+			</div>
+			<div className='form'><form action="">
 
-               <section >
-                        <label htmlFor="name">Nome fantasia: *</label>
-                        <input type="text" id='name' required/>
-               </section>
-                <section>
+				<h2>DADOS DE RECONHECIMENTO</h2>
+				<div className='form-bloco'>
+				<section>
+						<label htmlFor="razaoSocial">Razão social:</label>
+						<input type="text" name="" id="razaoSocial" required placeholder='Ex: Maria da Silva'/>
+					</section>
 
-                        <label htmlFor="razao">Razão Social:*</label>
-                        <input type="text" name="" id="razao" required/>
-                </section>
-                
-               
+					<section >
+						<label htmlFor="nomeFantasia">Nome fantasia:</label>
+						<input type="text" id='nomeFantasia' placeholder='Ex: Ateliê da Maria'/>
+					</section>
 
-                </div>
-                
-                
-         <div className='bloc2'>
-                    
-                    <section>
-                             <label htmlFor="cnpj">CNPJ:</label>
-                             <input type="number" name="" id="cnpj"/>
-                    </section>
-     
-                     <section>
-     
-                             <label htmlFor="datacpnj">Data CNPJ:</label>
-                             <input type="date" name="" id="datacpnj" />
-                     </section>
-                     <section>
-     
-                             <label htmlFor="inscricaoEs">Inscrição Estadual:*</label>
-                             <input type="number" name="" id="inscricaoEs" />
-                     </section>
-                     <section>
-     
-                             <label htmlFor="inscricaoMu">Inscrição Municipal:*</label>
-                             <input type="number" name="" id="nscricaoMu" />
-                     </section>
-                     <section>
-     
-     
-                             <label htmlFor="telefone">Telefone:</label>
-                             <input type="text" id='telefone' placeholder='(  )9 XXXX-XXXX' required/>
-                     </section>
-                     <section>
-     
-                             
-                             <label htmlFor="whats">Whatsapp:</label>
-                             <input type="text" name="" id="whats" placeholder='(  )9 XXXX-XXXX'/>
-                     </section>
-                     
-     
-                     </div>
+					<section >
+						<label htmlFor="responsavel">Responsável:</label>
+						<input type="text" id='responsavel' required/>
+					</section>
 
+					<section >
+						<label htmlFor="segmento">Segmento:</label>
+						<input type="text" id='segmento' required placeholder='Ex: Artesanato'/>
+					</section>
 
-                <div className='bloc3'>
+					<section>
+						<label htmlFor="atvPrincipal">Atividade principal:</label>
+						<input type="text" name="" id="atvPrincipal" required />
+					</section>
 
-                <section>
-                        <label htmlFor="endereco">Endereço:</label>
-                        <input type="text" name="" id="endereco" placeholder='Rua, avenida...' required/>
-                </section>
-                <section>
+					<section>
+						<label htmlFor="atvSecundaria">Atividade secundária:</label>
+						<input type="text" name="" id="atvSecundaria" placeholder='Caso haja...'/>
+					</section>
+				</div>
 
-                        <label htmlFor="bairro">Bairro:</label>
-                        <input type="text" name="" id="bairro" />
-                </section>
-                <section>
-                        <label htmlFor="atvPrincipal">Atividade principal:</label>
-                        <input type="text" name="" id="atvPrincipal" required/>
-                </section>
-                <section>
-                        <label htmlFor="atvSecundaria">Atividade secundária:</label>
-                        <input type="text" name="" id="atvSecundaria" />
-                </section>
-                </div>
-                <div className='bloc4'>
+				<h2>DADOS NUMÉRICOS</h2>
+				<div className='form-bloco'>
+					<section>
+						<label htmlFor="cnpj">CNPJ:</label>
+						<input type="number" name="" id="cnpj" placeholder='xx.xxx.xxx/xxxx-xx'/>
+					</section>
 
-                <section>
-                        <label htmlFor="categoria">Categoria de negócio:</label>
-                        <select name="" id="categoria">
-                            <option value="">Vestuário</option>
-                            <option value="">Gastronomia</option>
-                            <option value="">Mercantil</option>
-                            <option value="">Agronegócio</option>
-                            <option value="">Artesanato</option>
-                            <option value="">Casa agropecuária</option>
-                            <option value="">Decorações / festas</option>
-                            <option value="">Depositos e distribuidoras</option>
-                        </select>
-                </section>
-                 <section>
+					<section>
+						<label htmlFor="datacnpj">Inscrição no CNPJ:</label>
+						<input type="date" name="" id="datacpnj"/>
+					</section>
 
-                        <label htmlFor="situacao">Situação atual:</label>
-                        <select name="" id="situacao">
-                            <option value="true">Formal</option>
-                            <option value="false">Não formal</option>
-                            <option value="false">Em processo de formalização</option>
-                        </select>
-                       
-                </section> 
-                <section>
-                        <label htmlFor="nameEmpresario">Nome do empresário:</label>
-                        <input type="text" name="" id="nameEmpresario" />
-                </section>
-                </div>
-                <section className='buttonCadas'>
-                    <button>SALVAR INFORMAÇÕES</button>
-                </section>
+					<section>
+						<label htmlFor="inscriEstadual">Inscrição Estadual:</label>
+						<input type="number" name="" id="inscriEstadual" />
+					</section>
 
-            </form>
-        </div>
-        
-        </div>
-  )
-    
+					<section>
+						<label htmlFor="inscriMunicipal">Inscrição Municipal:</label>
+						<input type="number" name="" id="inscriMunicipal" />
+					</section>
+
+					<section>
+						<label htmlFor="telefoneCom">Telefone comercial:</label>
+						<input type="text" id='telefoneCom' placeholder='(xx) 9 xxxx-xxxx' required />
+					</section>
+
+					<section>
+						<label htmlFor="telefoneWhats">Telefone (Whatsapp):</label>
+						<input type="text" name="" id="telefoneWhats" placeholder='(xx) 9 xxxx-xxxx' />
+					</section>
+				</div>
+
+				<h2>DADOS DE LOGRADOURO COMERCIAL</h2>
+				<div className='form-bloco'>
+					<section>
+						<label htmlFor="endereco">Rua/Avenida:</label>
+						<input type="text" name="" id="endereco" placeholder='' required />
+					</section>
+					<section>
+
+						<label htmlFor="bairro">Bairro/Distrito:</label>
+						<input type="text" name="" id="bairro" />
+					</section>
+					
+				</div>
+				<div className='form-bloco'>
+
+					<section>
+						<label htmlFor="categoria">Categoria de negócio:</label>
+						<select name="" id="categoria">
+							<option value="">Vestuário</option>
+							<option value="">Gastronomia</option>
+							<option value="">Mercantil</option>
+							<option value="">Agronegócio</option>
+							<option value="">Artesanato</option>
+							<option value="">Casa agropecuária</option>
+							<option value="">Decorações / festas</option>
+							<option value="">Depositos e distribuidoras</option>
+						</select>
+					</section>
+					<section>
+
+						<label htmlFor="situacao">Situação atual:</label>
+						<select name="" id="situacao">
+							<option value="true">Formal</option>
+							<option value="false">Não formal</option>
+							<option value="false">Em processo de formalização</option>
+						</select>
+
+					</section>
+					<section>
+						<label htmlFor="nameEmpresario">Nome do empresário:</label>
+						<input type="text" name="" id="nameEmpresario" />
+					</section>
+				</div>
+				<section className='btn-cadastrar'>
+					<button><img src="/src/images/icones/icone-cadastrar.png" />SALVAR CADASTRO</button>
+				</section>
+
+			</form>
+			</div>
+
+		</div>
+	)
+
 }
